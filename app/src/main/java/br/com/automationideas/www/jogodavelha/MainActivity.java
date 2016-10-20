70410c41a19fe138c048157eeed37930af7a9f56
avyx <vinicius.avyx@gmail.com>
2016-10-20 21:38:38 -0200

    Criação do metedo clickQuadrado para recuparar na arquivo xml a tag de cada botão pressionado como também a criação da logica condicional para inferir qual simbolo vai ser impresso no botão.

--- app/src/main/java/br/com/automationideas/www/jogodavelha/MainActivity.java
+++ app/src/main/java/br/com/automationideas/www/jogodavelha/MainActivity.java
@@ -3,12 +3,16 @@ package br.com.automationideas.www.jogodavelha;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private View view;
-
-
+    private final String QUADRADO = "quadrado";
+    private final String BOLA = "O";
+    private final String XIS = "X";
+    private String lastPlay = "X";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
        setView(getLayoutInflater().inflate(R.layout.activity_main, null));
        setContentView(getView());
 
+    }
+
+    public void clickQuadrado(View v){
+
+
+        //constante da tag de cada botão
+        //ultilizamos essa constante para reculperar o botão atravez do metodo get quadrado
+        if(lastPlay.equals(XIS)) {
+            ((Button)v).setText(BOLA);
+            setLastPlay(BOLA);
+        }else {
+            ((Button)v).setText(XIS);
+            setLastPlay(XIS);
+        }
+
+        Toast.makeText(getView().getContext(),v.getTag().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    public Button getQuadrado (int tagNum){
+
+
+        return (Button)getView().findViewWithTag(QUADRADO+tagNum);
     }
 
     public View getView() {
@@ -28,4 +54,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public String getLastPlay() {
+        return lastPlay;
+    }
+
+    public void setLastPlay(String lastPlay) {
+        this.lastPlay = lastPlay;
+    }
 }
